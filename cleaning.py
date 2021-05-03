fcd2cf05c9bb05b598a4800440cbe959988c2312
Travis Andersen <andert714@gmail.com>
2021-05-03 09:47:41 -0600

    Concatenated houses, started eda

--- cleaning.py
+++ cleaning.py
@@ -1,8 +1,33 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from scraping import scrape_house
 
 urls = pd.read_csv('urls.csv')['url']
 df = pd.DataFrame()
 for url in urls:
-  house = scrape_house(url)
-  df = df.append(house)
+  try:
+    house = scrape_house(url)
+    df = df.append(house)
+  except:
+    pass
+
+df = pd.read_csv('data.csv')
+df = df.query('price < 1000000')
+# extra = df.iloc[:,86:]
+
+y = df.iloc[:,0]
+
+def eda(x, y):
+  info = {'name': x.name, 'unique': len(x.unique()), 'nas': sum(x.isna())}
+  print(info)
+  plt.scatter(x, y)
+  plt.show()
+
+  
+
+# Assuming Lasso or elastic ridge
+# Look for columns with many NAs or many of the same value
+# Check for outliers
+# Check for linearity (maybe transforming y)
+# Convert categorical variables
